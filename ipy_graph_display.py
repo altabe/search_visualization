0af992b9c3fb51dd8166df09611cab8851e3d88d
Tomer <tomer.altabe@gmail.com>
2021-06-14 05:10:29 +0000

    Added graph animating

--- ipy_graph_display.py
+++ ipy_graph_display.py
@@ -2,6 +2,7 @@ import tempfile
 import networkx as nx
 from networkx.drawing.nx_agraph import to_agraph
 from IPython.display import Image, display
+import time
 
 
 def display_graph(g):
@@ -10,3 +11,18 @@ def display_graph(g):
     with tempfile.NamedTemporaryFile(suffix='.png') as f:
         A.draw(f.name)
         display(Image(f.name))
+
+class GraphAnimator():
+    def __init__(self, plotting_interval_seconds=1) -> None:
+        self._interval = plotting_interval_seconds
+        self._last_display_time = time.time()
+
+    def plot(self, G):
+        time_since_last_display = time.time() - self._last_display_time
+        time_to_sleep = self._interval - time_since_last_display
+        if time_to_sleep > 0:
+            time.sleep(time_to_sleep)
+        display_graph(G)
+        self._last_display_time = time.time()
+
+
